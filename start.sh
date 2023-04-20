b3d65f66a88c7091a2bcfffef0c591e7ec48be96
温建锋 <wenjianfeng@cloudwalk.com>
2023-04-20 10:36:54 +0800

    update

---
+++ start.sh
@@ -0,0 +1,5 @@
+# inference
+/vehicle/yckj3860/miniconda3/envs/py39/bin/python sample_wjf.py \
+    --init_from=gpt2 \
+    --start="What is the answer to life, the universe, and everything?" \
+    --num_samples=5 --max_new_tokens=100
